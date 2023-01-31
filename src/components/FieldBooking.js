9bc50786b9231d4baf37834ab8ee994965874a65
adediresolomon <Solomonoluwaseun933@gmail.com>
2023-01-31 14:59:47 +0000

    new

--- src/components/FieldBooking.js
+++ src/components/FieldBooking.js
@@ -1,33 +1,44 @@
-import LocationAndMapLocationPin1 from "./assets/LocationAndMapLocationPin1";
-import BusinessCalendar from "./assets/BusinessCalendar";
-import BusinessCalendar1 from "./assets/BusinessCalendar1";
-import UsersAccount from "./assets/UsersAccount";
-import LocationAndMapLocationPin from "./assets/LocationAndMapLocationPin";
-import "./styles.css"
-export default function FieldBooking(props) {
-  return (
-    <>
-      <div className="w-full text-left font-Aeonik text-rgba(77,77,77,1) field-booking">
-        <div className="px-4 h-16 pt-1 pb-0.5 relative bg-white border-solid border gap-0.5 flex flex-col items-start rounded-lg w-243.1999969482422px border-rgba(77,77,77,1) field-booking-inner">
-          <p className="m-0 text-xs font-medium leading-normal">
-            {props.text}
-          </p>
-          <div className="flex items-center self-stretch justify-between w-full font-normal">
-            <p className="m-0 text-base leading-normal">{props.text1}</p>
-            {props.type === "DEFAULT_TYPE1" && <LocationAndMapLocationPin1 />}
-            {props.type === "DEFAULT_TYPE2" && <BusinessCalendar />}
-            {props.type === "DEFAULT_TYPE3" && <BusinessCalendar1 />}
-            {props.type === "DEFAULT_TYPE4" && <UsersAccount />}
-            {props.type === "DEFAULT_TYPE" && <LocationAndMapLocationPin />}
-          </div>
-        </div>
-      </div>
-    </>
-  );
-}
 
-FieldBooking.defaultProps = {
-  type: "DEFAULT_TYPE",
-  text: "From Where",
-  text1: "City or Airport"
-};
+import FlightTakeoffOutlinedIcon from '@mui/icons-material/FlightTakeoffOutlined';
+
+
+
+export default function FieldBooking() {
+	return (
+		<div className="w-full text-left font-poppins field-booking items-center">
+			<div className="px-4 h-16 pt-1 pb-0.5 relative  gap-0.5 items-start rounded-lg w-243.1999969482422px border-rgba(77,77,77,1) field-booking-inner">
+				<div class="warpper">
+					<input class="radio" id="one" name="group" type="radio" />
+					<input class="radio" id="two" name="group" type="radio" />
+					<input class="radio" id="three" name="group" type="radio" />
+					<div class="tabs grid gap-0 grid-rows lg:grid-cols-3 bg-white rounded-md p-2 ">
+						<label class="tab text-rose-900 bg-transparent hover:bg-rose-900 text-rose-900 font-semibold hover:text-white py-1 px-5 border border-rose-900 hover:border-transparent rounded" id="one-tab" for="one"><FlightTakeoffOutlinedIcon className="mr-2" /> One Way</label>
+						<label class="tab text-rose-900 bg-transparent hover:bg-rose-900 text-rose-900 font-semibold hover:text-white py-1 px-5 border border-rose-900 hover:border-transparent rounded" id="two-tab" for="two"><FlightTakeoffOutlinedIcon className="mr-2" /> Round Trip</label>
+						<label class="tab text-rose-900 bg-transparent hover:bg-rose-900 text-rose-900 font-semibold hover:text-white py-1 px-5 border border-rose-900 hover:border-transparent rounded" id="three-tab" for="three"><FlightTakeoffOutlinedIcon className="mr-2" /> Multi City</label>
+					</div>
+					<div class="panels">
+						<div class="panel" id="one-panel">
+							<div class="panel-title"></div>
+							<div className='grid grid-row lg: grid-cols-5 gap-1'>
+								<div>03333331</div>
+								<div>02</div>
+								<div>03</div>
+								<div>04</div>
+								<div>05</div>
+							</div>
+						</div>
+						<div class="panel" id="two-panel">
+							<div class="panel-title"></div>
+							<p></p>
+						</div>
+						<div class="panel" id="three-panel">
+							<div class="panel-title"></div>
+							<p></p>
+						</div>
+					</div>
+				</div>
+    </div>
+</div>
+
+ );
+}
