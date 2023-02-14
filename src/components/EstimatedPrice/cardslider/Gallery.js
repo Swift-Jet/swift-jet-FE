5ba16eb3662c6279d58d43fdde12d1e80f0fcba4
adediresolomon <Solomonoluwaseun933@gmail.com>
2023-02-14 14:48:38 +0000

    new update

--- src/components/EstimatedPrice/cardslider/Gallery.js
+++ src/components/EstimatedPrice/cardslider/Gallery.js
@@ -1,7 +1,7 @@
 export const images = [
     {
-      original: 'https://picsum.photos/id/1024/1000/600/',
-      thumbnail: 'https://picsum.photos/id/1024/250/150/',
+      original: 'https://res.cloudinary.com/dzv98o7ds/image/upload/v1675091085/Frame_104_lq9pu4.png',
+      thumbnail: 'https://res.cloudinary.com/dzv98o7ds/image/upload/v1675091085/Frame_104_lq9pu4.png',
     },
     {
       original: 'https://picsum.photos/id/1025/1000/600/',
--- src/components/EstimatedPrice/estimatecard/Estimatecards.js
+++ src/components/EstimatedPrice/estimatecard/Estimatecards.js
@@ -21,15 +21,27 @@ export default function BasicGrid() {
                 <Grid container spacing={2}>
                     <Grid item xs={8}>
                         <Item>
-                            <div class="grid grid-cols-2 space-x-40  ">
-                                <div class="h-auto w-full rounded-lg">
-                                   <Cardslider />
-                                </div>
-                                <div class=" h-auto w-1/2 rounded-lg">
-                                wivbwivbiwbvibvwij
-                                </div>
-                            
+                        <div class="m-auto max-w-6xl ">
+                        <div class="flex flex-col md:flex-row">
+                            <div class="md:w-3/5 max-w-3/5 flex flex-col justify-center">
+                                <div class=""><Cardslider /></div>  
                             </div>
+                            <div class="flex md:justify-start w-full md:w-1/2 -mt-9">
+                                <div class="flex ">
+                                    <div class=" max-w-sm mt-9 ">
+                                    <h1 class="md:text-xl text-xl uppercase font-black mt-11">Beachcraft</h1>
+                                    <h1 class="text-md mt-1 flex md:justify-start">Turboprop</h1>
+                                    <div className="justify-start flex">
+                                        <h1 className="md:text-3xl text-xl text-rose-900 mt-2 font-black">N150,000</h1>
+                                        
+                                    </div>
+                                    </div>
+                                    
+                                </div>
+                                
+                            </div>
+                        </div>
+                        </div>
                         </Item>
                     </Grid>
                     <Grid item xs={4}>
