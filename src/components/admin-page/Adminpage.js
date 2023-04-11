d1f41eaa4e0dbbdb790a2125ed549a8e709c710d
--replace-all <solomonoluwaseun933@gmail.com>
2023-04-11 22:39:31 +0100

    Dashboard update

--- src/components/admin-page/Adminpage.js
+++ src/components/admin-page/Adminpage.js
@@ -10,7 +10,7 @@ const Adminpage = () => {
    
     return (
  
-        <div class="container">
+        <div class="xl:container">
         <div class="navigation">
             <Navigation/>
         </div>
@@ -81,7 +81,7 @@ const Adminpage = () => {
                 </div>
             </div>
 
-    ]
+    
             <div class="details">
                 <div class="recentOrders">
                     <div class="cardHeader">
@@ -101,56 +101,56 @@ const Adminpage = () => {
 
                         <tbody>
                             <tr>
-                                <td>Star Refrigerator</td>
+                                <td>Aircarft</td>
                                 <td>$1200</td>
                                 <td>Paid</td>
                                 <td><span class="status delivered">Delivered</span></td>
                             </tr>
 
                             <tr>
-                                <td>Dell Laptop</td>
+                                <td>Aircarft</td>
                                 <td>$110</td>
                                 <td>Due</td>
                                 <td><span class="status pending">Pending</span></td>
                             </tr>
 
                             <tr>
-                                <td>Apple Watch</td>
+                                <td>Aircarft</td>
                                 <td>$1200</td>
                                 <td>Paid</td>
                                 <td><span class="status return">Return</span></td>
                             </tr>
 
                             <tr>
-                                <td>Addidas Shoes</td>
+                                <td>Aircarft</td>
                                 <td>$620</td>
                                 <td>Due</td>
                                 <td><span class="status inProgress">In Progress</span></td>
                             </tr>
 
                             <tr>
-                                <td>Star Refrigerator</td>
+                                <td>Aircarft</td>
                                 <td>$1200</td>
                                 <td>Paid</td>
                                 <td><span class="status delivered">Delivered</span></td>
                             </tr>
 
                             <tr>
-                                <td>Dell Laptop</td>
+                                <td>Aircarft</td>
                                 <td>$110</td>
                                 <td>Due</td>
                                 <td><span class="status pending">Pending</span></td>
                             </tr>
 
                             <tr>
-                                <td>Apple Watch</td>
+                                <td>Aircarft</td>
                                 <td>$1200</td>
                                 <td>Paid</td>
                                 <td><span class="status return">Return</span></td>
                             </tr>
 
                             <tr>
-                                <td>Addidas Shoes</td>
+                                <td>Aircarft</td>
                                 <td>$620</td>
                                 <td>Due</td>
                                 <td><span class="status inProgress">In Progress</span></td>
@@ -159,7 +159,7 @@ const Adminpage = () => {
                     </table>
                 </div>
 
-              ]
+              
                 <div class="recentCustomers">
                     <div class="cardHeader">
                         <h2>Recent Customers</h2>
